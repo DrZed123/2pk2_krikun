91eb61d73e52a7933c20ca8ecd71f80246580480
krikun.vv1689 <krikun.vv1689@M409C-4L>
2022-09-29 11:24:05 +0500

    Добавьте файлы проекта.

---
+++ PZ.5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+{
+    int a = -50;
+    int b = 50;
+
+    for (; a <= b; a += 10)
+    {
+        Console.WriteLine(a.ToString());
+    }
+    Console.ReadKey();
+}
+
+
+ 
---
+++ PZ_6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PZ_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = 1;
+            int result = 0;
+            while (true)
+            {
+                if (result < n - k * k)
+                {
+                    result = k * k;
+                    k++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(k + 1);
+        }
+    }
+}
